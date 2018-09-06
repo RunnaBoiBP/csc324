2704e7a822dc3ae4383dba5e63170536f49ba20a
RunnaBoiBP <benpetersonxc@gmail.com>
2018-09-06 14:22:22 -0400

    CH02_HW_Submit

--- ch02_ben_peterson.js
+++ ch02_ben_peterson.js
@@ -4,28 +4,28 @@
  * 
  ****************************************/
 
- for (let i=1;i<=7;i++){
-     console.log("#".repeat(i));
- }
+for (let i = 1; i <= 7; i++) {
+  console.log("#".repeat(i));
+}
 
- /****************************************
-  * 
-  * Exercise 2
-  * 
-  ****************************************/
+/****************************************
+ * 
+ * Exercise 2
+ * 
+ ****************************************/
 
-  /* Well, for starters I probably need a for loop with n going from 1 to 100. Then I should
-  * probably have the count of n go up each iteration of the loop. Then, if n is divisible by 3, it
-  * should print "Fizz", and if n is divisible by 5, it should print "Buzz". The hint says that the ||
-  * operator should be used, and when I googled it I came across using it in the console.log function
-  * to show the number or words depending on if it is divisible or not. */
+/* Well, for starters I probably need a for loop with n going from 1 to 100. Then I should
+* probably have the count of n go up each iteration of the loop. Then, if n is divisible by 3, it
+* should print "Fizz", and if n is divisible by 5, it should print "Buzz". The hint says that the ||
+* operator should be used, and when I googled it I came across using it in the console.log function
+* to show the number or words depending on if it is divisible or not. */
 
-  for (let n=1; n<=100;n++){
-      let output = "";
-      if (n%3==0) output += "Fizz";
-      if (n%5==0) output += "Buzz";
-      console.log(output||n);
-  }
+for (let n = 1; n <= 100; n++) {
+  let output = "";
+  if (n % 3 == 0) output += "Fizz";
+  if (n % 5 == 0) output += "Buzz";
+  console.log(output || n);
+}
 
 /******************************************
 *
