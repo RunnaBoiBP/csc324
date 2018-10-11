52559348cb67c62cf8104baa47f8885c5773f985
RunnaBoiBP <benpetersonxc@gmail.com>
2018-10-11 15:03:59 -0400

    prettied up and final submit of chapter 6 hw

--- ch06_ben_peterson.js
+++ ch06_ben_peterson.js
@@ -31,7 +31,10 @@ class Group {
         this.members = this.members.filter(x => x !== val);
     }
     // The delete function takes a value as an argument and filters the members array
-    // so that the remaining values do not equal the value passed as an argument.
+    // so that the remaining values do not equal the value passed as an argument. If the
+    // element returns true for not equaling the value, then the function keeps the
+    // element for the array. If the element returns false upon evaluation, then the 
+    // filter function does not include the element inside the array. 
     has(val) {
         return this.members.includes(val);
     }
@@ -48,32 +51,52 @@ class Group {
     // the collection, adding them to a new group created for the function. I had to
     // look in the book solutions for the syntax of this, I got the new group and the 
     // addition of the values, I just had trouble with what it meant by static from
-    // function. 
+    // function. Basically, static methods are called on the class itself, so by using
+    // the Group.from function, it creates a new instance of the array using the constructor
+    // function. It would need to be an iterable object, such as an array in this case.
+    // The from method then would iterate through the collection and use the add method
+    // to add the value at the current index of the collection if it is not currently in
+    // the members of the new group instance.
 }
 
 // Here is the code to test the class, with a lot of text to explain and analyze:
 
-let group = Group.from([10, 20]);
 console.log("Exercise 1: Groups Class\n");
-console.log("Using my class' has method, I should get a return value of true\n");
+console.log("This will be a nice, pretty test of my Group class method, with comments to allow for comprehension of what is going on.\n");
+console.log("First, let's test the constructor method\n");
+let csc324 = new Group;
+console.log("Running code 'let csc324 = new Group;'...\n");
+console.log(csc324);
+console.log("As you can see, we have an empty array inside.\n");
+console.log("Now, let's create a group array with actual values inside\n");
+let group = Group.from([10, 20]);
+console.log("Running code 'let group = Group.from[10, 20]);'...\n");
+console.log(group.members + "\n");
+console.log("Now, using my class' has method, I should get a return value of true\n");
 console.log(group.has(10) + "\n");
+console.log("Running code 'group.has(10)'...\n");
 console.log("As you can see, it works as intended\n");
 console.log("Now, let's test a value that isn't in the members array containing 10 and 20, such as 30\n");
 console.log(group.has(30) + "\n");
+console.log("Running code 'group.has(30)'...\n");
 console.log("So the has method clearly works in this case\n");
 console.log("Now, let's test the add method. I will use it to add a value of 10\n");
 group.add(10);
+console.log("Running code 'group.add(10)'...\n");
 console.log("Let's see what the group looks like now, shall we?\n");
 console.log(group.members + "\n");
 console.log("Since 10 is already in the members array, it doesn't get added per the function definition\n");
 console.log("Let's try adding in that 30 we were looking for earlier\n");
 group.add(30);
-console.log("Okay, let's test and see what's in the group members now\n");
+console.log("Running code 'group.add(30)'...\n");
+console.log("Okay, let's check and see what's in the group members now\n");
 console.log(group.members + "\n");
 console.log("Now, let's try out the delete method\n");
 group.delete(10);
+console.log("Running code 'group.delete(10)'...\n");
 console.log("Let's check the array now to see if 10 is still there\n");
 console.log(group.has(10) + "\n");
+console.log("Running code 'group.has(10)'...\n");
 console.log("As you can see, the has function returned false, meaning 10 is not a member of the array\n");
 console.log("Just to double check, let's print out the members of the array to be certain\n");
 console.log(group.members + "\n");
